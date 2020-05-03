f33bfd0398077b6942145340e69e85d593a113af
godcong <jumbycc@163.com>
2020-05-03 19:25:12 +0800

    test tcp

--- console/server/server.go
+++ console/server/server.go
@@ -52,7 +52,7 @@ func handleUDP() {
 		fmt.Println(err)
 		return
 	}
-	log.Printf("本地地址: <%s> \nn", listener.LocalAddr().String())
+	log.Printf("Local Addr: <%s> \nn", listener.LocalAddr().String())
 	log.Printf("Remote Addr:<%s> \n", listener.RemoteAddr().String())
 	peers := make([]net.UDPAddr, 0, 2)
 	data := make([]byte, 1024)
