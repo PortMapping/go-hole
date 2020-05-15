15e2feccf4601166a8e95f79899a1ce3e0d044a6
godcong <jumbycc@163.com>
2020-05-15 18:08:52 +0800

    update lurker

--- tcp_listener.go
+++ tcp_listener.go
@@ -1,1 +1,88 @@
 package lurker
+
+import (
+	"context"
+	"fmt"
+	"github.com/portmapping/go-reuse"
+	"net"
+
+	p2pnat "github.com/libp2p/go-nat"
+	"github.com/portmapping/lurker/nat"
+)
+
+type tcpListener struct {
+	ctx         context.Context
+	cancel      context.CancelFunc
+	source      chan Source
+	port        int
+	mappingPort int
+	nat         nat.NAT
+	tcpListener net.Listener
+	cfg         *Config
+}
+
+func newTCPListener(cfg *Config) Listener {
+	tcp := &tcpListener{
+		ctx:    nil,
+		cancel: nil,
+		port:   cfg.TCP,
+		cfg:    cfg,
+		source: make(chan Source),
+	}
+	tcp.ctx, tcp.cancel = context.WithCancel(context.TODO())
+	return tcp
+}
+
+// Listen ...
+func (l *tcpListener) Listen() (c <-chan Source, err error) {
+	tcpAddr := LocalTCPAddr(l.port)
+	if l.cfg.Secret != nil {
+		l.tcpListener, err = reuse.ListenTLS("tcp", DefaultLocalTCPAddr.String(), l.cfg.Secret)
+	} else {
+		l.tcpListener, err = reuse.ListenTCP("tcp", tcpAddr)
+	}
+	if err != nil {
+		return nil, err
+	}
+	go listenTCP(l.ctx, l.tcpListener, l.source)
+
+	if !l.cfg.NAT {
+		return l.source, nil
+	}
+
+	l.nat, err = nat.FromLocal(l.cfg.TCP)
+	if err != nil {
+		log.Debugw("nat error", "error", err)
+		if err == p2pnat.ErrNoNATFound {
+			fmt.Println("listen tcp on address:", tcpAddr.String())
+		}
+		l.cfg.NAT = false
+	} else {
+		extPort, err := l.nat.Mapping()
+		if err != nil {
+			log.Debugw("nat mapping error", "error", err)
+			l.cfg.NAT = false
+			return l.source, nil
+		}
+		l.mappingPort = extPort
+
+		address, err := l.nat.GetExternalAddress()
+		if err != nil {
+			log.Debugw("get external address error", "error", err)
+			l.cfg.NAT = false
+			return l.source, nil
+		}
+		addr := ParseSourceAddr("tcp", address, extPort)
+		fmt.Println("mapping on address:", addr.String())
+	}
+	return
+}
+
+// Stop ...
+func (l *tcpListener) Stop() error {
+	if l.cancel != nil {
+		l.cancel()
+		l.cancel = nil
+	}
+	return nil
+}
