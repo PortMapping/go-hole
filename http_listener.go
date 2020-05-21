8e6b512d0a4b74fb7f0dfa8a8f0ad01813462cfe
godcong <jumbycc@163.com>
2020-05-21 14:01:10 +0800

    update lurker

--- http_listener.go
+++ http_listener.go
@@ -2,6 +2,8 @@ package lurker
 
 import (
 	"context"
+	"fmt"
+	p2pnat "github.com/libp2p/go-nat"
 	"github.com/portmapping/go-reuse"
 	"github.com/portmapping/lurker/nat"
 	"net"
@@ -20,6 +22,11 @@ type httpListener struct {
 	srv         *http.Server
 }
 
+// MappingPort ...
+func (l *httpListener) MappingPort() int {
+	return l.mappingPort
+}
+
 // Listen ...
 func (l *httpListener) Listen(c chan<- Source) (err error) {
 	tcpAddr := LocalTCPAddr(l.port)
@@ -32,8 +39,37 @@ func (l *httpListener) Listen(c chan<- Source) (err error) {
 		return err
 	}
 	l.srv = &http.Server{Handler: l.handler}
+	fmt.Println("listen http on address:", tcpAddr.String())
 	go listenHTTP(l.ctx, l.srv, l.tcpListener, c)
+	if !l.cfg.NAT {
+		return nil
+	}
 
+	l.nat, err = nat.FromLocal("tcp", l.cfg.TCP)
+	if err != nil {
+		log.Debugw("nat error", "error", err)
+		if err == p2pnat.ErrNoNATFound {
+			//fmt.Println("listen tcp on address:", tcpAddr.String())
+		}
+		l.cfg.NAT = false
+	} else {
+		extPort, err := l.nat.Mapping()
+		if err != nil {
+			log.Debugw("nat mapping error", "error", err)
+			l.cfg.NAT = false
+			return nil
+		}
+		l.mappingPort = extPort
+
+		address, err := l.nat.GetExternalAddress()
+		if err != nil {
+			log.Debugw("get external address error", "error", err)
+			l.cfg.NAT = false
+			return nil
+		}
+		addr := ParseSourceAddr("tcp", address, extPort)
+		fmt.Println("http mapping on address:", addr.String())
+	}
 	return
 }
 
