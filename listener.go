f65db7806e1827952803a9b362756b305caac438
godcong <jumbycc@163.com>
2020-05-25 17:02:58 +0800

    update lurker

--- listener.go
+++ listener.go
@@ -1,10 +1,23 @@
 package lurker
 
+import "github.com/portmapping/lurker/nat"
+
 // Listener ...
 type Listener interface {
 	Listen() (err error)
-	MappingPort() int
 	Stop() error
 	IsReady() bool
 	Accept() <-chan Connector
+}
+
+// PortMapping ...
+type PortMapping interface {
+	IsSupport() bool
+	NAT() nat.NAT
+}
+
+// MappingListener ...
+type MappingListener interface {
+	Listener
+	PortMapping
 }
