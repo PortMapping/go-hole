66bcd9bba72d96fa2e1b6d4cc27b70eafa9ef742
godcong <jumbycc@163.com>
2020-05-28 16:29:40 +0800

    update lurker

--- console/client.go
+++ console/client.go
@@ -5,6 +5,7 @@ import "github.com/spf13/cobra"
 func cmdClient() *cobra.Command {
 	tcp := 0
 	udp := 0
+	local := 0
 	cmd := &cobra.Command{
 		Use: "client",
 		Run: func(cmd *cobra.Command, args []string) {
@@ -13,5 +14,6 @@ func cmdClient() *cobra.Command {
 	}
 	cmd.Flags().IntVarP(&tcp, "tcp", "t", 16004, "handle tcp port")
 	cmd.Flags().IntVarP(&udp, "udp", "u", 16005, "handle udp port")
+	cmd.Flags().IntVarP(&local, "local", "l", 16004, "handle local mapping port")
 	return cmd
 }
