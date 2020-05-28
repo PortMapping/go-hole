174c453607b562f9baaef6aa0436d7256254a6cb
godcong <jumbycc@163.com>
2020-05-28 16:57:50 +0800

    update lurker

--- console/server.go
+++ console/server.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+
+	"github.com/portmapping/lurker"
+	"github.com/spf13/cobra"
+)
 
 func cmdServer() *cobra.Command {
 	tcp := 0
@@ -8,7 +13,25 @@ func cmdServer() *cobra.Command {
 	cmd := &cobra.Command{
 		Use: "server",
 		Run: func(cmd *cobra.Command, args []string) {
-			// Do Stuff Here
+			cfg := lurker.DefaultConfig()
+			cfg.TCP = tcp
+			cfg.UDP = udp
+
+			l := lurker.New(cfg)
+			t := lurker.NewTCPListener(cfg)
+			l.RegisterListener("tcp", t)
+			connectors, err := l.Listen()
+			if err != nil {
+				panic(err)
+				return
+			}
+
+			fmt.Println("your connect id:", lurker.GlobalID)
+			go func() {
+				for connector := range connectors {
+					fmt.Println(connector.ID())
+				}
+			}()
 		},
 	}
 	cmd.Flags().IntVarP(&tcp, "tcp", "t", 16004, "handle tcp port")
