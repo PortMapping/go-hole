b18e518a003ce82157da64d7916ed2fdab1cfa01
godcong <jumbycc@163.com>
2020-05-28 17:32:26 +0800

    update lurker

--- console/main.go
+++ console/main.go
@@ -2,17 +2,10 @@ package main
 
 import (
 	"fmt"
+	"os/signal"
 
-	"net"
-	"os"
-	"strconv"
-	"sync"
-	"time"
-
-	"github.com/portmapping/lurker"
-	"github.com/portmapping/lurker/common"
-	"github.com/portmapping/lurker/nat"
 	"github.com/spf13/cobra"
+	"os"
 )
 
 var rootCmd = &cobra.Command{
@@ -24,6 +17,12 @@ var rootCmd = &cobra.Command{
 	},
 }
 
+func waitForSignal() {
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, os.Interrupt)
+	<-sigs
+}
+
 func main() {
 
 	rootCmd.AddCommand(cmdServer(), cmdClient())
@@ -31,96 +30,7 @@ func main() {
 	if err := rootCmd.Execute(); err != nil {
 		return
 	}
-	return
-	network := "tcp"
-	address := ""
-	list := sync.Map{}
-	if len(os.Args) > 2 {
-		network = os.Args[1]
-		address = os.Args[2]
-	}
-	listen := true
-	if len(os.Args) > 3 {
-		parseBool, err := strconv.ParseBool(os.Args[3])
-		if err != nil {
-			listen = true
-		} else {
-			listen = parseBool
-		}
-
-	}
-
-	cfg := lurker.DefaultConfig()
-	cfg.TCP = 16004
-	//cfg.UDP = 16005
-
-	l := lurker.New(cfg)
-	err := lurker.RegisterLocalProxy(l, cfg)
-	if err != nil {
-		return
-	}
-	tcpPort, udpPort := 0, 0
-	if listen {
-		fmt.Println("listen with port mapping")
-		t := lurker.NewTCPListener(cfg)
-		//u := lurker.NewUDPListener(cfg)
-		l.RegisterListener("tcp", t)
-		//l.RegisterListener("udp", u)
-		connectors, err := l.Listen()
-		if err != nil {
-			panic(err)
-			return
-		}
-
-		fmt.Println("your connect id:", lurker.GlobalID)
-		go func() {
-			for connector := range connectors {
-				fmt.Println(connector.ID())
-			}
-		}()
-	} else {
-		nat, err := nat.FromLocal("tcp", l.Config().TCP)
-		if err != nil {
-			fmt.Println("error tcpport", err)
-		}
-		err = nat.Mapping()
-		if err != nil {
-			fmt.Println("error tcpport", err)
-		}
-		tcpPort = nat.ExtPort()
-	}
-	if len(os.Args) > 2 {
-		addr, i := common.ParseAddr(address)
-		localAddr := net.IPv4zero
-		ispAddr := net.IPv4zero
-
-		fmt.Println("remote common:", addr.String(), i)
-		s := lurker.NewSource(lurker.Service{
-			ID:      lurker.GlobalID,
-			ISP:     ispAddr,
-			Local:   localAddr,
-			PortUDP: l.Config().UDP,
-			PortTCP: l.Config().TCP,
-		}, common.Addr{
-			Protocol: network,
-			IP:       addr,
-			Port:     i,
-		})
-		s.SetMappingPort("tcp", tcpPort)
-		s.SetMappingPort("udp", udpPort)
-		go func() {
-			_, ok := list.Load(s.Service().ID)
-			if ok {
-				return
-			}
-			err := s.Connect()
-			fmt.Println("target connected:", err)
-			if err != nil {
-				list.Store(s.Service().ID, s)
-			}
-		}()
-
-	}
 	fmt.Println("ready for waiting")
-	time.Sleep(30 * time.Minute)
+	waitForSignal()
+	return
 }
