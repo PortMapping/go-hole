5b6952094d75df6db91c4a865b0f7e9117b26fec
godcong <jumbycc@163.com>
2020-05-31 00:03:03 +0800

    update lurker

--- lurker_test.go
+++ lurker_test.go
@@ -8,14 +8,14 @@ import (
 
 // TestParseAddr ...
 func TestParseAddr(t *testing.T) {
-	addr, i := addr.ParseAddr("192.168.0.0:1234")
+	addr, i := common.ParseAddr("192.168.0.0:1234")
 	if strings.Compare("192.168.0.0", addr.String()) != 0 {
 		t.Fatal(addr.String(), i)
 	}
 	if i != 1234 {
 		t.Fatal(addr.String(), i)
 	}
-	addr, i = addr.ParseAddr("192.168.0.0")
+	addr, i = common.ParseAddr("192.168.0.0")
 	if strings.Compare("192.168.0.0", addr.String()) != 0 {
 		t.Fatal(addr.String(), i)
 	}
